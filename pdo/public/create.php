66cac10ee661c72a5e14edcd32580c4a1f83362c
Emily Seay <eseay3157@my.sullivan.edu>
2019-08-29 19:17:56 -0400

    working on create.php

--- pdo/public/create.php
+++ pdo/public/create.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Use an HTML form to create a new entry in the
  * users table.
@@ -11,21 +10,21 @@ require "../common.php";
 
 if (isset($_POST['submit'])) {
   if (!hash_equals($_SESSION['csrf'], $_POST['csrf'])) die();
-
   try  {
     $connection = new PDO($dsn, $username, $password, $options);
-    
+
     $new_user = array(
+      "UserID"  => $_POST ['UserID'],
      "FirstName" => $_POST['FirstName'],
-      "LastName"  => $_POST['LastName'],
-      "UserID"     => $_POST['UserID'],
-      "HouseID"       => $_POST['HouseID'],
-      "LocationID"  => $_POST['LocationID']
+      "LastName" => $_POST['LastName'],
+      "HouseID"  => $_POST['HouseID'],
+      "ActivityID"  => $_POST['ActivityID']
+      "LocationID" => $_POST['LocationID']
     );
 
     $sql = sprintf(
       "INSERT INTO %s (%s) values (%s)",
-      "users",
+      "Hogwarts",
       implode(", ", array_keys($new_user)),
       ":" . implode(", :", array_keys($new_user))
     );
@@ -40,21 +39,23 @@ if (isset($_POST['submit'])) {
 <?php require "templates/header.php"; ?>
 
   <?php if (isset($_POST['submit']) && $statement) : ?>
-    <blockquote><?php echo escape($_POST"FirstName"]); ?> successfully added.</blockquote>
+    <blockquote><?php echo escape($_POST['FirstName']); ?> successfully added.</blockquote>
   <?php endif; ?>
 
   <h2>Add a user</h2>
 
   <form method="post">
     <input name="csrf" type="hidden" value="<?php echo escape($_SESSION['csrf']); ?>">
+    <label for="UserID">User ID</label>
+    <input type="text" name="UserID" id="UserID">
     <label for="FirstName">First Name</label>
     <input type="text" name="FirstName" id="FirstName">
     <label for="LastName">Last Name</label>
     <input type="text" name="LastName" id="LastName">
-    <label for="UserID">User ID</label>
-    <input type="text" name="UserID" id="UserID">
     <label for="HouseID">HouseID</label>
     <input type="text" name="HouseID" id="HouseID">
+    <label for="ActivityID">ActivityID</label>
+    <input type="text" name="ActivityID" id="ActivityID">
     <label for="LocationID">LocationID</label>
     <input type="text" name="LocationID" id="LocationID">
     <input type="submit" name="submit" value="Submit">
